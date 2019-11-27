2905f83127c8455ab0ad828a97a6e40a4997b27d
RickyRajinder <singh.sangh@gmail.com>
2019-11-26 18:46:52 -0800

    add yaml config for mult-switch parsing

---
+++ parse_config.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import glob
+import yaml
+
+config = {}
+
+file = glob.glob('*.yml')
+file.extend(glob.glob('*.yaml'))
+if len(file) != 1:
+    print("Config file could not be found")
+    sys.exit(1)
+
+with open(file[0]) as f:
+    try:
+        config = yaml.safe_load(f)
+    except yaml.YAMLError as e:
+        print(e)
+
+with open(os.path.abspath("./install/boot/cmdline.txt"), "a") as f:
+    f.write(str(config['server_address']).rstrip("\n"))
