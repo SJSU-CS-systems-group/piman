11db7bd7bb6238703277f3c6c493b91e1ea434d3
ben reed <ben.reed@sjsu.edu>
2020-09-21 16:45:27 -0700

    use boot files from the raspberry pi repo
    instead of copying the boot files from the raspberry pi repo,
    we use the repo as a submodule.

--- make_zipapp.sh
+++ make_zipapp.sh
@@ -11,7 +11,17 @@ cp -r logging.conf utility tftp dhcp monitoring tcp *.py build/piman.app
 mkdir build/piman.app/install
 mkdir build/logs
 cp piman.yaml build
-cp -r install/boot build/piman.app/install
+#git clone --depth 1 --single-branch https://github.com/raspberrypi/firmware.git firmware
+if [ ! -f firmware/boot/bootcode.bin ]
+then
+    git submodule init firmware
+    git submodule update --depth 1 firmware
+fi
+
+
+mkdir -p build/piman.app/install/boot
+cp -r firmware/boot build/piman.app/install/
+cp -r install/boot build/piman.app/install/
 cp -r config_ui build/piman.app/config_ui
 # we don't want crypto stuff since it has native code
 rm -r build/piman.app/Crypto*
